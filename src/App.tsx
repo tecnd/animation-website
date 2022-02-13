e46631a71360f06d3412f6a15f5466b106b203f0
Kerry Wang <zwang34@email.wm.edu>
2022-02-13 17:37:29 -0500

    added video element

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import planets1 from './videos/planets1.mp4'
 
+function App() {
   return (
     <div className="text-center bg-zinc-800 h-screen flex flex-col justify-center items-center text-white text-3xl gap-2">
-      <img src={logo} className="h-96 animate-[spin_20s_linear_infinite] pointer-events-none" alt="logo" />
       <p>Hello Vite + React + Tailwind!</p>
-      <p>
-        <button className="px-2 bg-gray-400 hover:bg-blue-800 text-white rounded-md" onClick={() => setCount((count) => count + 1)}>
-          count is: {count}
-        </button>
-      </p>
       <p>
         Edit <code>App.tsx</code> and save to test HMR updates.
       </p>
+      <video controls src={planets1}/>
       <p>
         <a
           className="text-blue-400"
