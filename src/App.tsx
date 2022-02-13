6196bc358359ac83002675f4d30d08fc2f470de1
Kerry Wang <zwang34@email.wm.edu>
2022-02-13 14:22:43 -0500

    Converted css to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,36 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button className="px-3 bg-gray-400 text-white rounded-md" type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="text-center bg-zinc-800 h-screen flex flex-col justify-center items-center text-white text-3xl gap-2">
+      <img src={logo} className="h-96 animate-[spin_20s_linear_infinite]" alt="logo" />
+      <p>Hello Vite + React + Tailwind!</p>
+      <p>
+        <button className="px-2 bg-gray-400 hover:bg-blue-800 text-white rounded-md" type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+      <p>
+        Edit <code>App.tsx</code> and save to test HMR updates.
+      </p>
+      <p>
+        <a
+          className="text-blue-400"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {' | '}
+        <a
+          className="text-blue-400"
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+      </p>
     </div>
   )
 }
