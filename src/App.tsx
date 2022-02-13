9fc35fd8d9a28cc4a5b29ee940d7007774d1eec3
Kerry Wang <zwang34@email.wm.edu>
2022-02-13 14:28:29 -0500

    fixed rotating image blocking button

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
 
   return (
     <div className="text-center bg-zinc-800 h-screen flex flex-col justify-center items-center text-white text-3xl gap-2">
-      <img src={logo} className="h-96 animate-[spin_20s_linear_infinite]" alt="logo" />
+      <img src={logo} className="h-96 animate-[spin_20s_linear_infinite] pointer-events-none" alt="logo" />
       <p>Hello Vite + React + Tailwind!</p>
       <p>
-        <button className="px-2 bg-gray-400 hover:bg-blue-800 text-white rounded-md" type="button" onClick={() => setCount((count) => count + 1)}>
+        <button className="px-2 bg-gray-400 hover:bg-blue-800 text-white rounded-md" onClick={() => setCount((count) => count + 1)}>
           count is: {count}
         </button>
       </p>
