e2898120f15d804c569cb3e66329c6a41ed12f73
Kerry Wang <kwzeyunwang@gmail.com>
2022-02-13 20:43:44 -0500

    Added content to Card prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 
 import Card from './Card'
+import planetsThumb from './images/planets.png'
 import planets1 from './videos/planets1.mp4'
 
 function App() {
   return (
-    <div className="py-5 h-screen flex flex-wrap gap-5 justify-center items-center">
-      <Card/>
-    </div>
+    <>
+      <header>
+        <h1 className="text-white text-3xl text-center py-5">CSCI 417 Computer Animation</h1>
+      </header>
+      <div className="py-5 h-screen flex flex-wrap gap-5 justify-center items-center">
+        <Card image={planetsThumb} title="Solar System"/>
+      </div>
+    </>
   );
 }
 
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,13 @@
-function Card(props) {
+export declare interface CardProps {
+    image: string,
+    title: string
+}
+
+function Card(props: CardProps) {
     return(
-        <div className="bg-white hover:bg-slate-300 rounded-md h-72 w-60 shrink-0">
-            {props.children}
+        <div className="bg-white hover:bg-slate-300 rounded-md border-2 h-72 w-60 shrink-0">
+            <img src={props.image} className="rounded-t-md"/>
+            <h2 className="p-2 text-2xl text-black">{props.title}</h2>
         </div>
     )
 }
