f459e97249b757fd6bb92dc70a4aabbcc33d5fa1
Kerry Wang <kwzeyunwang@gmail.com>
2022-03-24 13:39:13 -0400

    added compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { imagetools } from "vite-imagetools";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), imagetools()],
+  plugins: [
+    react(),
+    imagetools(),
+    viteCompression(),
+    viteCompression({ algorithm: "brotliCompress", ext: ".br" }),
+  ],
   base: "/~zwang34/",
 });
